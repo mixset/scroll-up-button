1f92573f001398272cc015a0f9cdec52a21b882c
Dominik Ryńko <dominikrynko@gmail.com>
2018-03-04 17:47:23 +0100

    Initial commit v1.0

---
+++ scripts.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+
+ // Show scrollToTop button or not
+ $(window).scroll(function() {
+  if($(this).scrollTop() > 200) {
+   $('.scrollup').fadeIn();
+  } 
+  else
+  {
+   $('.scrollup').fadeOut();
+  }
+ });
+
+ // Fluent scrolling to top
+ $('.scrollup').click(function() {
+  $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+ });	 
+ 
+});
